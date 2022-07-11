8d2e710fafe35abe35ee304ce4fbf382f1ecf6fd
Marco <clam@sps-program.com>
2022-07-11 19:32:44 +0000

    Change

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -16,7 +16,7 @@ public class MyController {
     @GetMapping("/")
     List<String> getTest(){
         Datastore datastore =DatastoreOptions.newBuilder()
-        .setProjectId("sekhrivijayrcs1")
+        .setProjectId("just-strength-326117")
         .build().getService();
         //Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
